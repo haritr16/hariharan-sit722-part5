5a537b8e3057b5fb9c2dab55487dbb9637e9cf62
haritr16 <s223295149@deakin.edu.au>
2024-10-03 18:44:12 +1000

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,14 @@
+# Your Solution
 #!/bin/bash
 
-#kubectl delete -f kubernetes/deployment.yaml
-envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f -
+if [ -z "$CONTAINER_REGISTRY" ]; then
+  echo "Error: VERSION is not set."
+  exit 1
+fi
 
+if [ -z "$VERSION" ]; then
+  echo "Error: VERSION is not set."
+  exit 1
+fi
+
+envsubst < ./scripts/kubernetes/deployment.yaml | kubectl apply -f -
--- scripts/terraform/variable.tf
+++ scripts/terraform/variable.tf
@@ -1,6 +1,6 @@
 
 variable "app_name" {
-  default = "sit722projecthariharan"
+  default = "sit722projecttask5hari"
 }
 
 variable "location" {
