e665a531703a012d1126d1ef2afc880d6172e819
haritr16 <s223295149@deakin.edu.au>
2024-10-04 04:05:46 +1000

    update

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,15 +1,5 @@
 # Your Solution
 #!/bin/bash
 
-if [ -z "$CONTAINER_REGISTRY" ]; then
-  echo "Error: VERSION is not set."
-  exit 1
-fi
-
-if [ -z "$VERSION" ]; then
-  echo "Error: VERSION is not set."
-  exit 1
-fi
-
 docker push $CONTAINER_REGISTRY.azurecr.io/book_catalog:$VERSION
 docker push $CONTAINER_REGISTRY.azurecr.io/inventory_management:$VERSION
