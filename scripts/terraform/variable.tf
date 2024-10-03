289cb9e7aba19d74c17ed5dba3c1a03e2a4f75e9
haritr16 <s223295149@deakin.edu.au>
2024-10-03 13:44:10 +1000

    update loc

--- scripts/terraform/variable.tf
+++ scripts/terraform/variable.tf
@@ -5,3 +5,15 @@ variable "resource_group_name" {
 variable "location" {
   default = "East US"
 }
+
+variable "app_name" {
+  default = "sit722proj5"
+}
+
+variable "location" {
+  default = "australiasoutheast"
+}
+
+variable "kubernetes_version" {
+  default = "1.30.3"
+}
